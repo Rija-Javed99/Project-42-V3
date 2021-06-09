73ac6829e9165925e40f22d8e3f583c527a27ef9
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-06-09 17:53:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,23 +27,22 @@ function draw() {
   if(!hasDocked){
     spacecraft.x = spacecraft.x + random(-1,1);
     
-  if(keyDown("UP_ARROW")){
-    spacecraft.y = spacecraft.y -2;
-  }
+ 
+    //add conditions
     
-  if(keyDown("LEFT_ARROW")){
-      spacecraft.addImage(scimg3);
-    spacecraft.x = spacecraft.x - 1;
-  }
     
-  if(keyDown("RIGHT_ARROW")){
-      spacecraft.addImage(scimg2);
-    spacecraft.x = spacecraft.x + 1;
-  }
     
-  if(keyDown("DOWN_ARROW")){
-      spacecraft.addImage(scimg1);
-  }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
   if(spacecraft.y <= (iss.y+70) && spacecraft.x <= (iss.x-10)){
     hasDocked = true;
